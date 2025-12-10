53bde41487ce156e801c2556d303b4d1f05a62bb
Tharan <cbtharan976@gmail.com>
2025-12-11 04:31:33 +0530

    Updated Weird Algorithm code

---
+++ weird_algorithm.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    while (true) {
+        cout << n;
+        if (n == 1) break;
+        cout << " ";
+
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+
+    return 0;
+}
