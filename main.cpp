45ad6f2be4b5021430a7af80a5548afc13a7fa9e
Tharan <cbtharan976@gmail.com>
2025-12-21 07:42:29 +0530

    optimized missing and repeating number

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
-    cout << "C++ setup successful!";
+    long long n;
+    cin >> n;
+
+    while (true) {
+        cout << n;             // print current value
+
+        if (n == 1) break;     // stop condition
+
+        cout << " ";           // space between numbers
+
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+
     return 0;
 }
