2a35c028e7ea5ed42e6f650c6541c87864496119
Tharan <cbtharan976@gmail.com>
2026-03-17 10:43:17 +0530

    Cses solution for string reorder

--- palindrome_reorder.cpp
+++ palindrome_reorder.cpp
@@ -1,23 +1,43 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    string s;
-    cin >> s;
-
-    // count frequency
-    unordered_map<char,int> freq;
-    for (char c : s) freq[c]++;
-
-    // count odd frequencies
-    int odd = 0;
-    for (auto &p : freq) {
-        if (p.second & 1) odd++;
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main(){
+        string s;
+        cin>>s;
+        
+        unordered_map<char,int> freq;//AAABB
+        for(char ch: s){
+            freq[ch]++;
+        }
+        
+        char center='0';
+        int oddcount=0;
+        
+         for(const auto& [ch,count] : freq){
+            if(count % 2 == 1){
+                oddcount++;
+                center = ch;
+    
+                if(oddcount > 1){
+                    cout << "NO SOLUTION";
+                    return 0;
+                }
+            }
+        }
+        string result;
+        result.reserve(s.length()); 
+        
+        for(const auto& [ch,count]: freq){
+            result.append(count/2,ch);//3*J
+        }
+        
+        if(center!='0'){
+            result+=center;
+        }
+        
+        int firsthalfsize=s.length()/2;
+        
+        for(int i=firsthalfsize-1;i>=0;i--){
+            result.push_back(result[i]);
+        }
+        cout<<result;
     }
-
-    cout << (odd <= 1 ? "Yes\n" : "No\n");
-    return 0;
-}
