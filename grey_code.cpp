299741217fd5d7238e5a73f16d27d0bbfef738b6
Tharan <cbtharan976@gmail.com>
2026-03-21 15:08:52 +0530

    Solution for grey code in CSES

--- grey_code.cpp
+++ grey_code.cpp
@@ -1,24 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to generate Gray Codes
 vector<string> generate_gc(int n) {
-    
-    // Base case
     if (n == 1) {
         return {"0", "1"};
     }
 
-    // Get (n-1) Gray codes
     vector<string> prev = generate_gc(n - 1);
 
-    // Step 1: reverse
-    vector<string> mirrored;
-    for (int i = (int)prev.size() - 1; i >= 0; i--) {
-        mirrored.push_back(prev[i]);
-    }
-
-    // Step 2: build result
     vector<string> result;
 
     // Add '0' prefix
@@ -26,31 +15,22 @@ vector<string> generate_gc(int n) {
         result.push_back("0" + prev[i]);
     }
 
-    // Add '1' prefix (to reversed)
-    for (size_t i = 0; i < mirrored.size(); i++) {
-        result.push_back("1" + mirrored[i]);
+    // Add '1' prefix in reverse
+    for (int i = (int)prev.size() - 1; i >= 0; i--) {
+        result.push_back("1" + prev[i]);
     }
 
     return result;
 }
 
-void solve() {
+int main() {
     int n;
     cin >> n;
 
     vector<string> gray_codes = generate_gc(n);
 
     for (const auto &gc : gray_codes) {
-        cout << gc << endl;
-    }
-}
-
-int main() {
-    int t;
-    cin >> t;
-
-    while (t--) {
-        solve();
+        cout << gc << "\n";
     }
 
     return 0;
