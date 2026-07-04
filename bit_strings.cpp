368f363de8efa1ae3b304fc44de29c048f192ffe
wick-976 <cbtharan976@gmail.com>
2026-07-04 07:56:17 +0530

    Bit strings

--- bit_strings.cpp
+++ bit_strings.cpp
@@ -1,52 +1,34 @@
+//explanationn is given in the binary exponentiation.cpp file
+
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-
-const ll MOD = 1000000007;
-
-// Naive approach: O(n)
-ll naive_2powN(ll N) {
-    ll ans = 1;
-    for (ll i = 0; i < N; i++) {
-        ans = (ans * 2) % MOD;
-    }
-    return ans;
-}
-
-// Binary Exponentiation: O(log N)
-ll optimised_apowb(ll a, ll b) {
-    ll res = 1;
-
-    while (b > 0) {
-
-        // If the current bit of b is 1
-        if (b & 1) {
-            res *= a;
-            res %= MOD;
-        }
-
-        // Square the base
-        a = (a * a) % MOD;
-
-        // Move to the next bit
-        b >>= 1;
-    }
-
-    return res;
-}
-
-void solve() {
-    ll n;
+int main() {
+    long long n;
     cin >> n;
 
-    cout << optimised_apowb(2, n) << "\n";
-}
+    const long long MOD = 1000000007;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    long long a = 2;
+    long long res = 1;
 
-    solve();
+    while (n > 0) {
+
+        // If the current bit is 1,
+        // collect the current power
+        if (n & 1) {
+            res = (res * a) % MOD;
+        }
+
+        // Produce the next power:
+        // 2^1 -> 2^2 -> 2^4 -> 2^8 ...
+        a = (a * a) % MOD;
+
+        // Move to the next binary bit
+        n >>= 1;
+    }
+
+    cout << res << '\n';
+
     return 0;
 }
