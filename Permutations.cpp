b67adf82ab1d8dac9e4fc2cb50a084f1ad33e6bb
wick-976 <cbtharan976@gmail.com>
2026-08-20 04:44:42 +0530

    Permutaions

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,20 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    
-    if (n == 2 || n == 3) {
-        cout << "NO SOLUTION";
-        return 0;
+
+void permutataions(int n)
+{
+    if(n == 1)
+    {
+        cout<<1;
+        return;
+    }
+    if(n<=3)
+    {
+        cout<<"NO SOLUTION";
+        return;
     }
     for(int i=2;i<=n;i+=2)
     {
-       cout<<i<<" ";
-    
-}
+        cout<<i<<" ";
+    }
     for(int i=1;i<=n;i+=2)
     {
         cout<<i<<" ";
     }
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    permutataions(n);
 }
--- missing_number.cpp
+++ missing_number.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
